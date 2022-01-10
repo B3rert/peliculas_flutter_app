739dc9d13c37f003ad507e8fc5517e22a603d419
Robert L <robertmorales181@gmail.com>
2022-01-10 17:33:43 -0600

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,15 +10,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'Peliculas',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'details': (_) => const DetailsScreen(),
+      },
     );
   }
 }
