f1e4d61f5b57ff390c4bc1c2436fe6d6c952297e
Robert L <robertmorales181@gmail.com>
2022-01-13 09:13:04 -0600

    infinite scroll in movie slider

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/models/models.dart';
 
-class MovieSlider extends StatelessWidget {
+class MovieSlider extends StatefulWidget {
   const MovieSlider({
     Key? key,
     required this.movies,
     this.title,
+    required this.onNextPage,
   }) : super(key: key);
 
   final List<Movie> movies;
   final String? title;
+  final Function onNextPage;
+
+  @override
+  State<MovieSlider> createState() => _MovieSliderState();
+}
+
+class _MovieSliderState extends State<MovieSlider> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          (_scrollController.position.maxScrollExtent - 500)) {
+        widget.onNextPage();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      height: 260,
+      height: 270,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (title != null)
+          if (widget.title != null)
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Text(
-                title!,
+                widget.title!,
                 style: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -35,10 +61,11 @@ class MovieSlider extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
+              controller: _scrollController,
               scrollDirection: Axis.horizontal,
-              itemCount: movies.length,
+              itemCount: widget.movies.length,
               itemBuilder: (_, int index) => _MoviePoster(
-                movie: movies[index],
+                movie: widget.movies[index],
               ),
             ),
           ),
@@ -80,7 +107,7 @@ class _MoviePoster extends StatelessWidget {
           ),
           const SizedBox(height: 5),
           Text(
-            movie.originalTitle,
+            movie.title,
             overflow: TextOverflow.ellipsis,
             maxLines: 2,
             textAlign: TextAlign.center,
