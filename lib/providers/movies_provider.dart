4d0af2c7b4f4535a3bf23f700d43ec6091e6a4f0
Robert L <robertmorales181@gmail.com>
2022-01-18 13:55:20 -0600

    search model response

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -17,7 +17,7 @@ class MoviesProvider extends ChangeNotifier {
   int _popularPage = 0;
 
   Future<String> _getJsonData(String endpoint, [int page = 1]) async {
-    var url = Uri.https(
+    final url = Uri.https(
       _baseUrl,
       endpoint,
       {
@@ -67,5 +67,23 @@ class MoviesProvider extends ChangeNotifier {
     final creditsResponse = CreditsResponse.fromJson(jsonData);
     movieCast[movieId] = creditsResponse.cast;
     return creditsResponse.cast;
+  }
+
+  Future<List<Movie>> searchMovies(String query) async {
+    final urlSearch = Uri.https(
+      _baseUrl,
+      '/3/search/movie',
+      {
+        'api_key': _apiKey,
+        'language': _language,
+        'page': "1",
+        'query': query,
+      },
+    );
+
+    var response = await http.get(urlSearch);
+
+    final searchResponse = SearchResponse.fromJson(response.body);
+    return searchResponse.results;
   }
 }
---
+++ lib/search/search_delegate.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MovieSearchDelegate extends SearchDelegate {
+  @override
+  String get searchFieldLabel => 'Buscar';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () => query = '',
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () => close(context, null),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if (query.isEmpty) {
+      return const Center(
+        child: Icon(
+          Icons.movie_creation_rounded,
+          size: 130,
+          color: Colors.black38,
+        ),
+      );
+    }
+
+    return Container();
+  }
+}
