ee76a04802f0a2e8a8278d705133f049aa25b6ed
Robert L <robertmorales181@gmail.com>
2022-01-19 11:46:37 -0600

    seacrh added

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/models/models.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
@@ -32,16 +35,71 @@ class MovieSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+
     if (query.isEmpty) {
-      return const Center(
-        child: Icon(
-          Icons.movie_creation_rounded,
-          size: 130,
-          color: Colors.black38,
-        ),
-      );
+      return const _EmptyContainer();
     }
 
-    return Container();
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+        final movies = snapshot.data;
+
+        if (!snapshot.hasData) return const _EmptyContainer();
+        return ListView.builder(
+          itemCount: movies?.length,
+          itemBuilder: (_, int index) {
+            return _MovieItem(
+              movie: movies![index],
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  const _MovieItem({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: const AssetImage('assets/no-image.jpg'),
+        image: NetworkImage(movie.fullProsterImg),
+        width: 50.0,
+        fit: BoxFit.contain,
+      ),
+      title: Text(movie.title),
+      subtitle: Text(movie.originalTitle),
+      onTap: () {
+        // close(context, null);
+        Navigator.pushNamed(context, 'details', arguments: movie);
+      },
+    );
+  }
+}
+
+class _EmptyContainer extends StatelessWidget {
+  const _EmptyContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Icon(
+        Icons.movie_creation_rounded,
+        size: 130,
+        color: Colors.black38,
+      ),
+    );
   }
 }
